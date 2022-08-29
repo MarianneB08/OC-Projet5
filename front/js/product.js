d960c2c5a370de4ba071f5d8bb0ec385854e26a0
Marianne Boyer <mboyer.correction.relecture@gmail.com>
2022-08-29 17:18:07 +0200

    product.js : ajout des informations 'title', 'price', 'item__img' et 'description' sur chaque page Product

--- front/js/product.js
+++ front/js/product.js
@@ -1,8 +1,6 @@
 const url = window.location.href;
 const urlProduct = new URL(url);
 let idProduct = "";
-
-
 let search_params = new URLSearchParams(urlProduct.search);
 
 if(search_params.has('id')) {
@@ -17,14 +15,10 @@ fetch(`http://localhost:3000/api/products/${idProduct}`)
     let itemInfos;
     for(let i in products){
         itemInfos = i;
-        console.log(itemInfos, products[i]);
-        //console.log(itemInfos, products[i].name);
-        document.querySelector('#title').innerHTML = (itemInfos, products[i].name);
-        document.querySelector('#price').innerHTML = (itemInfos, products[i].price);
-        document.querySelector('#description').innerHTML = (itemInfos, products[i].description);
-        document.querySelector('.item__img').innerHTML = `<img src="${(itemInfos, products[i].imageUrl)}" alt="${itemInfos, products[i].altTxt}">`;
+        document.querySelector('#title').innerHTML = (itemInfos, products.name);
+        document.querySelector('#price').innerHTML = (itemInfos, products.price);
+        document.querySelector('#description').innerHTML = (itemInfos, products.description);
+        document.querySelector('.item__img').innerHTML = `<img src="${(itemInfos, products.imageUrl)}" alt="${itemInfos, products.altTxt}">`;
     }
-}).catch(function(erreur) {
-    console.log("erreur!");
 })
 
