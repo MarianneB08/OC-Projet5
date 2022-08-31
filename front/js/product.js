4714c2a55ee43849dd243e9929661185250b3a4b
Marianne Boyer <mboyer.correction.relecture@gmail.com>
2022-08-31 15:41:35 +0200

    products.js : ajout du sélectionner de couleurs pour chaque produit // products.js et script.js : ajout de commentaires dans le code //

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,7 @@
+/////////////// AFFICHER LE PRODUIT SUR LA PAGE PRODUIT ///////////////
+
+// On récupère l'id du produit dans l'url de la page produit
+
 const url = window.location.href;
 const urlProduct = new URL(url);
 let idProduct = "";
@@ -7,6 +11,8 @@ if(search_params.has('id')) {
     idProduct = search_params.get('id');
 };
 
+// On appelle l'API pour récupérer les caractéristiques du produit
+
 fetch(`http://localhost:3000/api/products/${idProduct}`)
 .then(function (response) {
     return response.json();
@@ -15,10 +21,19 @@ fetch(`http://localhost:3000/api/products/${idProduct}`)
     let itemInfos;
     for(let i in products){
         itemInfos = i;
+        // On injecte les caractéristiques dans la structure HTML
         document.querySelector('#title').innerHTML = (itemInfos, products.name);
         document.querySelector('#price').innerHTML = (itemInfos, products.price);
         document.querySelector('#description').innerHTML = (itemInfos, products.description);
         document.querySelector('.item__img').innerHTML = `<img src="${(itemInfos, products.imageUrl)}" alt="${itemInfos, products.altTxt}">`;
     }
+    const colorArray = products.colors;
+    for(let color of colorArray) {
+        document.querySelector('#colors').innerHTML += `<option value="${color}"> ${color}</option>`
+    }
 })
+
+
+
+/////////////// AJOUTER LE PRODUIT ET SES CARACTÉRISTIQUES AU LOCALSTORAGE ///////////////
 
--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,5 @@
+/////////////// AFFICHER LES PRODUITS SUR LA PAGE D'ACCUEIL ///////////////
+
 let products = [];
 
 // Récupération des données de l'API
@@ -7,9 +9,10 @@ fetch("http://localhost:3000/api/products")
     })
     .then(function(products) {
         for(let product in products) {
+            // On injecte les caractéristiques de chaque produit dans une card
             document.querySelector(".items")
             .innerHTML +=`<a href="./product.html?id=${products[product]._id}">
-            <article>
+            <article> 
               <img src=${products[product].imageUrl} alt=${products[product].altTxt}>
               <h3 class="productName">${products[product].name}</h3>
               <p class="productDescription">${products[product].description}</p>
